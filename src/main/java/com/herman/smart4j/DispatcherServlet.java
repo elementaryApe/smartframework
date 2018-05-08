42ed6343933b0a78084dc9804eb4cc267427984c
韩世豪 <hsh@wdtianxia.com>
2018-05-08 11:16:10 +0800

    1.优化ServletApi，达到解耦

--- src/main/java/com/herman/smart4j/DispatcherServlet.java
+++ src/main/java/com/herman/smart4j/DispatcherServlet.java
@@ -5,11 +5,8 @@ import com.herman.smart4j.bean.Handler;
 import com.herman.smart4j.bean.Param;
 import com.herman.smart4j.bean.View;
 import com.herman.smart4j.helper.*;
-import com.herman.smart4j.utils.CodecUtil;
 import com.herman.smart4j.utils.JsonUtil;
 import com.herman.smart4j.utils.ReflectionUtil;
-import com.herman.smart4j.utils.StreamUtil;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.ServletConfig;
@@ -23,8 +20,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -52,62 +47,68 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //获取请求方法与请求路径
-        String requestMethod = req.getMethod().toLowerCase();
-        String requestPath = req.getPathInfo();
-        if(StringUtils.equals(requestPath,"/favicon.ico")){
-            return;
-        }
-        // 获取Action 处理器
-        Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
-        if (handler != null) {
-            //获取Controller类及其Bean实例
-            Class<?> controllerClass = handler.getControllerClass();
-            Object controllerBean = BeanHelper.getBean(controllerClass);
-            //创建请求参数对象
-            //request中获取参数对象 判断是否为上传请求
-            Param param;
-            if (UploadHelper.isMultipart(req)) {
-                param = UploadHelper.createParam(req);
-            } else {
-                param = RequestHelper.createParam(req);
+        ServletHelper.init(req,resp);
+        try {
+
+            //获取请求方法与请求路径
+            String requestMethod = req.getMethod().toLowerCase();
+            String requestPath = req.getPathInfo();
+            if (StringUtils.equals(requestPath, "/favicon.ico")) {
+                return;
             }
-            Object result ;
-            //调用Action
-            Method actionMethod = handler.getActionMethod();
-            if (param.isEmpty()) {
-                result = ReflectionUtil.invokeMethod(controllerBean, actionMethod);
-            } else {
-                result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
-            }
-            if (result instanceof View) {
-                View view = (View) result;
-                String path = view.getPath();
-                if (StringUtils.isNotEmpty(path)) {
-                    if (path.startsWith("/")) {
-                        resp.sendRedirect(req.getContextPath() + path);
-                    } else {
-                        Map<String, Object> model = view.getModel();
-                        for (Map.Entry<String, Object> entry : model.entrySet()) {
-                            req.setAttribute(entry.getKey(), entry.getValue());
+            // 获取Action 处理器
+            Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
+            if (handler != null) {
+                //获取Controller类及其Bean实例
+                Class<?> controllerClass = handler.getControllerClass();
+                Object controllerBean = BeanHelper.getBean(controllerClass);
+                //创建请求参数对象
+                //request中获取参数对象 判断是否为上传请求
+                Param param;
+                if (UploadHelper.isMultipart(req)) {
+                    param = UploadHelper.createParam(req);
+                } else {
+                    param = RequestHelper.createParam(req);
+                }
+                Object result;
+                //调用Action
+                Method actionMethod = handler.getActionMethod();
+                if (param.isEmpty()) {
+                    result = ReflectionUtil.invokeMethod(controllerBean, actionMethod);
+                } else {
+                    result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
+                }
+                if (result instanceof View) {
+                    View view = (View) result;
+                    String path = view.getPath();
+                    if (StringUtils.isNotEmpty(path)) {
+                        if (path.startsWith("/")) {
+                            resp.sendRedirect(req.getContextPath() + path);
+                        } else {
+                            Map<String, Object> model = view.getModel();
+                            for (Map.Entry<String, Object> entry : model.entrySet()) {
+                                req.setAttribute(entry.getKey(), entry.getValue());
+                            }
+                            req.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(req, resp);
                         }
-                        req.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(req, resp);
+                    }
+                } else if (result instanceof Data) {
+                    //返回JSON数据
+                    Data data = (Data) result;
+                    Object model = data.getModel();
+                    if (model != null) {
+                        resp.setContentType("application/json");
+                        resp.setCharacterEncoding("UTF-8");
+                        PrintWriter writer = resp.getWriter();
+                        String json = JsonUtil.toJson(model);
+                        writer.write(json);
+                        writer.flush();
+                        writer.close();
                     }
                 }
-            } else if (result instanceof Data) {
-                //返回JSON数据
-                Data data = (Data) result;
-                Object model = data.getModel();
-                if (model != null) {
-                    resp.setContentType("application/json");
-                    resp.setCharacterEncoding("UTF-8");
-                    PrintWriter writer = resp.getWriter();
-                    String json = JsonUtil.toJson(model);
-                    writer.write(json);
-                    writer.flush();
-                    writer.close();
-                }
             }
+        }finally {
+            ServletHelper.destroy();
         }
 
     }
